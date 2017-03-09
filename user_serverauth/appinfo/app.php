b3037711ff27ffb819177930dbef978ef2d577c4
Daniel Klaffenbach <daniel.klaffenbach@hrz.tu-chemnitz.de>
2017-03-09 09:10:23 +0100

    Revert "Work around NextCloud autoloader issue"
    This reverts commit 649d50d2959fccc84b15390547e20bcbfbfdbf41. This way
    the master branch can be used to reproduce the Nextcloud autoloader
    issue.

--- user_serverauth/appinfo/app.php
+++ user_serverauth/appinfo/app.php
@@ -9,10 +9,6 @@
  */
 require_once OC_App::getAppPath('user_serverauth').'/user_serverauth.php';
 
-// This is a hack to work around NextCloud issue #3771.
-// It is not clear to me why this particular class is not autoloaded by the class loader.
-require_once OC::$SERVERROOT."/lib/private/Memcache/ArrayCache.php";
-
 $userBackend = new \OCA\ServerAuth\ServerAuthBackend(\OC::$server->getSession());
 OC_User::useBackend($userBackend);
 OC_User::handleApacheAuth();
